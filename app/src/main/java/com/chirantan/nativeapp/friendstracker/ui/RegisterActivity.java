a8a5a05e4d0ba82a624c8460560267bd7b434a78
chirantandeveloper <mygamingac91@gmail.com>
2020-06-05 23:09:53 +0530

    login

---
+++ app/src/main/java/com/chirantan/nativeapp/friendstracker/ui/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.chirantan.nativeapp.friendstracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chirantan.nativeapp.friendstracker.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
